5b5f646874ec07d4da25d21f908115aa9fa39c82
sadaam74 <sadaamabdi993@gmail.com>
2024-11-02 13:43:25 -0700

    add remove'not found'

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -2,7 +2,7 @@ const { readTasksFromile } = require("../utils/fileHandler")
 
 exports.getTasks = (req, res)=>{
     const tasks = readTasksFromfile();
-    res.writeHead(200,'not found',{'content-type':'appliction/json'})
+    res.writeHead(200,{'content-type':'appliction/json'})
         res.end(JSON.stringify(tasks))
 
 }
