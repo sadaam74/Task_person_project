7e63371fb24c435d6e6e0a5560a0d2e9a97f49b6
sadaam74 <sadaamabdi993@gmail.com>
2024-11-02 11:51:40 -0700

    changing p to port

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { prototype } = require('events');
 const http = require('http');
 const HOSTNAME = 'localhost'
 const PORT = 9000
@@ -13,7 +14,7 @@ const server = http.createServer((req,res)=>{
     }
 
 });
-server.listen(P,HOSTNAME,()=>{
+server.listen(PORT,HOSTNAME,()=>{
     console.log(`server running on port ${PORT}`)
 
 })
