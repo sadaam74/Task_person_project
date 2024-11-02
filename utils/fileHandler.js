89bfde00e6d0885b516e4f110ed08ddebae4110f
sadaam74 <sadaamabdi993@gmail.com>
2024-11-02 13:30:12 -0700

    add fileHandle.js

--- utils/fileHandler.js
+++ utils/fileHandler.js
@@ -4,3 +4,11 @@ const filepat = './dta/tasks.json';
 const writeTasksTofie = (tasks)=>{
     fs.writeFileSync(filepat,JSON.stringify(tasks))
 }
+const readTasksFromile = ()=>{
+    i (!fs.existsSync(filepath)){
+        writeTasksTofie([])
+    }
+
+const data = fs.readFileSync(filepat);
+return JSON.parse(data)
+}
