5d03fbc2ac92693f915ec7146fb064b8990d4c25
sadaam74 <sadaamabdi993@gmail.com>
2024-11-02 12:15:49 -0700

    teskRoutes.js

---
+++ routes/taskRoutes.js
@@ -0,0 +1,18 @@
+const taskRoutes = (req,res)=>{
+    if(req.method == 'get'){
+        getTask(req ,res);
+    }else if(req.method == 'post'){
+        createTask(req , res)
+        
+    }else if  (req.method == 'PATCH'){
+        updateTask(req , res)
+    }else if (req.method == 'DELETE'){
+        deleteTask(req,res)
+    }else {
+        res.writeHead(404,'not found',{'content-type':'appliction/json'})
+        res.end(JSON.stringify({
+            message: 'unknown method required.'
+
+        }))
+    }
+}
