4a1551b7a9df2158343122b08dc6e85b9f44e71e
sadaam74 <sadaamabdi993@gmail.com>
2024-11-02 13:41:03 -0700

    add taskController.js

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,6 +1,8 @@
 const { readTasksFromile } = require("../utils/fileHandler")
 
 exports.getTasks = (req, res)=>{
-    const tasks = readTasksFromile
+    const tasks = readTasksFromfile();
+    res.writeHead(404,'not found',{'content-type':'appliction/json'})
+        res.end(JSON.stringify(tasks))
 
 }
